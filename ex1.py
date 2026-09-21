490707538cdc0a5dcbec6b2c636ef5be50bdd758
Kojima Lucas <kojimalucas43@gmail.com>
2026-09-21 12:41:10 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,15 @@
+def calcularTotalGols(notas):
+    soma = 0
+    for nota in notas:
+        soma+=nota
+    return soma / len()
+
+def calcularMediaGols(gols):
+    soma = 0
+    for gols in gol:
+        soma+=nota
+    return soma / len()
+
+    notas = [8, 9,7]
+    media = calcularMediaGols(gols)
+
